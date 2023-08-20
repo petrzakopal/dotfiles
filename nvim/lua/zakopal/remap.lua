dc34a3b51455f3cf9e0d826c67a795b3532e2ecc
petrzakopal <petr.zakopal@gmail.com>
2023-08-20 13:17:50 +0200

    added remap for next and previous buffersw

--- nvim/lua/zakopal/remap.lua
+++ nvim/lua/zakopal/remap.lua
@@ -3,3 +3,9 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Prettier remap from :Prettier
 vim.keymap.set("n", "<leader>pr", vim.cmd.Prettier)
+
+
+
+vim.keymap.set("n", "<C-J>", vim.cmd.bnext)
+vim.keymap.set("n", "<C-K>", vim.cmd.bprev)
+
