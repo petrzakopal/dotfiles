ce2cfdca0af2a94285806f8c6bef0845381c4abd
Petr Zakopal <petr.zakopal@gmail.com>
2024-09-09 07:00:49 +0200

    nvim: adde tab shortcuts

--- nvim/lua/zakopal/remap.lua
+++ nvim/lua/zakopal/remap.lua
@@ -1,6 +1,16 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Default tabs in neovim
+-- New tab
+vim.keymap.set("n", "<leader>tn", vim.cmd.tabnew)
+-- Next tab
+vim.keymap.set("n", "<leader>tN", vim.cmd.tabnext)
+-- Previous tab
+vim.keymap.set("n", "<leader>tP", vim.cmd.tabprevious)
+-- Close a tab
+vim.keymap.set("n", "<leader>tc", vim.cmd.tabclose)
+
 -- Prettier remap from :Prettier
 --vim.keymap.set("n", "<leader>pr", vim.cmd.PrettierAsync)
 
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -188,14 +188,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: onedarkpro.nvim
-time([[Config for onedarkpro.nvim]], true)
-try_loadstring("\27LJ\2\n<\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\29colorscheme onedark_dark\bcmd\bvim\0", "config", "onedarkpro.nvim")
-time([[Config for onedarkpro.nvim]], false)
 -- Config for: nvim-autopairs
 time([[Config for nvim-autopairs]], true)
 try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
 time([[Config for nvim-autopairs]], false)
+-- Config for: onedarkpro.nvim
+time([[Config for onedarkpro.nvim]], true)
+try_loadstring("\27LJ\2\n<\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\29colorscheme onedark_dark\bcmd\bvim\0", "config", "onedarkpro.nvim")
+time([[Config for onedarkpro.nvim]], false)
 vim.cmd [[augroup packer_load_aucmds]]
 vim.cmd [[au!]]
   -- Filetype lazy-loads
