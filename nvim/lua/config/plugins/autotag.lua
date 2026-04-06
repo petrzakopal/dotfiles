afb1df2d9f75c56d3e75c43857cbe97710668f47
Petr Zakopal <Petr.Zakopal@gmail.com>
2026-04-06 16:22:01 +0200

    deleted autotag

--- nvim/lua/config/plugins/autotag.lua
+++
@@ -1,8 +0,0 @@
-
-    return{ {
-        'windwp/nvim-ts-autotag',
-        opt = true,
-        ft = 'typescriptreact',
-        requires = { { 'nvim-treesitter/nvim-treesitter' } },
-        config = function() require('nvim-ts-autotag').setup() end,
-    }}
