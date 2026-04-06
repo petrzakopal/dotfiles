401b4da25694847c3894cdbe2e46a4410443c1eb
Petr Zakopal <Petr.Zakopal@gmail.com>
2026-04-06 18:08:52 +0200

    fixing styles

--- nvim/lua/config/plugins/tokyonight.lua
+++ nvim/lua/config/plugins/tokyonight.lua
@@ -8,15 +8,21 @@ return { {
             transparent = true,
             styles = {
                 -- Disable background for all syntax elements
-                comments = { bg = "NONE" },
-                keywords = { bg = "NONE" },
-                functions = { bg = "NONE", bold = true },
-                variables = { bg = "NONE" },
-                strings = { bg = "NONE" },
-                numbers = { bg = "NONE" },
-                constants = { bg = "NONE" },
-                operators = { bg = "NONE" },
-                types = { bg = "NONE" },
+                --comments = { bg = "NONE" },
+                --keywords = { bg = "NONE" },
+                --functions = { bg = "NONE", bold = true },
+                --variables = { bg = "NONE" },
+                --strings = { bg = "NONE" },
+                --numbers = { bg = "NONE" },
+                --constants = { bg = "NONE" },
+                --operators = { bg = "NONE" },
+                --types = { bg = "NONE" },
+                --sidebars = "transparent",
+                --floats = "transparent",
+                comments = { italic = false },
+                keywords = { italic = false },
+                functions = { italic = false },
+                variables = { italic = false },
                 sidebars = "transparent",
                 floats = "transparent",
             },
@@ -28,55 +34,64 @@ return { {
                 --     end
                 -- end
 
-                hl.Comment = { bg = "NONE", fg = colors.comment }
-                hl.Keyword = { bg = "NONE", fg = colors.keyword }
-                hl.Function = { bg = "NONE", fg = colors.func, bold = true }
-                hl.Variable = { bg = "NONE", fg = colors.fg }
-                hl.String = { bg = "NONE", fg = colors.string }
-                hl.Number = { bg = "NONE", fg = colors.number }
-                hl.Boolean = { bg = "NONE", fg = colors.orange }
-                hl.Operator = { bg = "NONE", fg = colors.blue5 }
+                --hl.Comment = { bg = "NONE", fg = colors.comment }
+                --hl.Keyword = { bg = "NONE", fg = colors.keyword }
+                --hl.Function = { bg = "NONE", fg = colors.func, bold = true }
+                --hl.Variable = { bg = "NONE", fg = colors.fg }
+                --hl.String = { bg = "NONE", fg = colors.string }
+                --hl.Number = { bg = "NONE", fg = colors.number }
+                --hl.Boolean = { bg = "NONE", fg = colors.orange }
+                --hl.Operator = { bg = "NONE", fg = colors.blue5 }
 
-                -- TypeScript/JavaScript specific
-                hl.TSKeyword = { bg = "NONE", fg = colors.keyword }
-                hl.TSKeywordFunction = { bg = "NONE", fg = colors.keyword, bold = true }
-                hl.TSKeywordReturn = { bg = "NONE", fg = colors.keyword }
-                hl.TSKeywordOperator = { bg = "NONE", fg = colors.keyword }
-                hl.TSConstant = { bg = "NONE", fg = colors.purple }
-                hl.TSVariable = { bg = "NONE", fg = colors.fg }
-                hl.TSParameter = { bg = "NONE", fg = colors.red }
-                hl.TSConstructor = { bg = "NONE", fg = colors.blue1 }
-                hl.TSType = { bg = "NONE", fg = colors.blue1 }
-                hl.TSConditional = { bg = "NONE", fg = colors.keyword }
-                hl.TSRepeat = { bg = "NONE", fg = colors.keyword }
-                hl.TSLabel = { bg = "NONE", fg = colors.keyword }
+                --hl.Comment = { bg = "NONE", fg = colors.comment }
+                --hl.Keyword = { bg = "NONE", fg = colors.keyword }
+                --hl.Function = { bg = "NONE", fg = colors.func, bold = true }
+                --hl.Variable = { bg = "NONE", fg = colors.fg }
+                --hl.String = { bg = "NONE", fg = colors.string }
+                --hl.Number = { bg = "NONE", fg = colors.number }
+                --hl.Boolean = { bg = "NONE", fg = colors.orange }
+                --hl.Operator = { bg = "NONE", fg = colors.blue5 }
 
-                -- TreeSitter specific groups
-                hl["@keyword"] = { bg = "NONE", fg = colors.keyword }
-                hl["@keyword.function"] = { bg = "NONE", fg = colors.keyword, bold = true }
-                hl["@keyword.return"] = { bg = "NONE", fg = colors.keyword }
-                hl["@keyword.operator"] = { bg = "NONE", fg = colors.keyword }
-                hl["@constant"] = { bg = "NONE", fg = colors.purple }
-                hl["@constant.builtin"] = { bg = "NONE", fg = colors.purple }
-                hl["@variable"] = { bg = "NONE", fg = colors.fg }
-                hl["@constructor"] = { bg = "NONE", fg = colors.blue1 }
-                hl["@type"] = { bg = "NONE", fg = colors.blue1 }
+                ---- TypeScript/JavaScript specific
+                --hl.TSKeyword = { bg = "NONE", fg = colors.keyword }
+                --hl.TSKeywordFunction = { bg = "NONE", fg = colors.keyword, bold = true }
+                --hl.TSKeywordReturn = { bg = "NONE", fg = colors.keyword }
+                --hl.TSKeywordOperator = { bg = "NONE", fg = colors.keyword }
+                --hl.TSConstant = { bg = "NONE", fg = colors.purple }
+                --hl.TSVariable = { bg = "NONE", fg = colors.fg }
+                --hl.TSParameter = { bg = "NONE", fg = colors.red }
+                --hl.TSConstructor = { bg = "NONE", fg = colors.blue1 }
+                --hl.TSType = { bg = "NONE", fg = colors.blue1 }
+                --hl.TSConditional = { bg = "NONE", fg = colors.keyword }
+                --hl.TSRepeat = { bg = "NONE", fg = colors.keyword }
+                --hl.TSLabel = { bg = "NONE", fg = colors.keyword }
 
-                -- Keep selection highlights only
-                hl.Visual = { bg = colors.bg_visual }
-                hl.Search = { bg = colors.bg_search, fg = colors.fg }
-                hl.IncSearch = { bg = colors.orange, fg = colors.black }
+                ---- TreeSitter specific groups
+                --hl["@keyword"] = { bg = "NONE", fg = colors.keyword }
+                --hl["@keyword.function"] = { bg = "NONE", fg = colors.keyword, bold = true }
+                --hl["@keyword.return"] = { bg = "NONE", fg = colors.keyword }
+                --hl["@keyword.operator"] = { bg = "NONE", fg = colors.keyword }
+                --hl["@constant"] = { bg = "NONE", fg = colors.purple }
+                --hl["@constant.builtin"] = { bg = "NONE", fg = colors.purple }
+                --hl["@variable"] = { bg = "NONE", fg = colors.fg }
+                --hl["@constructor"] = { bg = "NONE", fg = colors.blue1 }
+                --hl["@type"] = { bg = "NONE", fg = colors.blue1 }
 
-                hl.TelescopeSelection = { bg = colors.bg_visual, fg = colors.fg }
-                hl.TelescopeSelectionCaret = { bg = colors.bg_visual, fg = colors.blue }
-                hl.TelescopeMultiSelection = { bg = colors.bg_visual, fg = colors.fg }
-                hl.TelescopeNormal = { bg = "NONE", fg = colors.fg }
-                hl.TelescopeBorder = { bg = "NONE", fg = colors.blue0 }
-                hl.TelescopePromptBorder = { bg = "NONE", fg = colors.blue0 }
-                hl.TelescopeResultsBorder = { bg = "NONE", fg = colors.blue0 }
-                hl.TelescopePreviewBorder = { bg = "NONE", fg = colors.blue0 }
-                hl.TelescopeMatching = { bg = "NONE", fg = colors.teal }
-                hl.TelescopePromptPrefix = { bg = "NONE", fg = colors.blue }
+                ---- Keep selection highlights only
+                --hl.Visual = { bg = colors.bg_visual }
+                --hl.Search = { bg = colors.bg_search, fg = colors.fg }
+                --hl.IncSearch = { bg = colors.orange, fg = colors.black }
+
+                --hl.TelescopeSelection = { bg = colors.bg_visual, fg = colors.fg }
+                --hl.TelescopeSelectionCaret = { bg = colors.bg_visual, fg = colors.blue }
+                --hl.TelescopeMultiSelection = { bg = colors.bg_visual, fg = colors.fg }
+                --hl.TelescopeNormal = { bg = "NONE", fg = colors.fg }
+                --hl.TelescopeBorder = { bg = "NONE", fg = colors.blue0 }
+                --hl.TelescopePromptBorder = { bg = "NONE", fg = colors.blue0 }
+                --hl.TelescopeResultsBorder = { bg = "NONE", fg = colors.blue0 }
+                --hl.TelescopePreviewBorder = { bg = "NONE", fg = colors.blue0 }
+                --hl.TelescopeMatching = { bg = "NONE", fg = colors.teal }
+                --hl.TelescopePromptPrefix = { bg = "NONE", fg = colors.blue }
             end,
         })
 
--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -1,41 +1,41 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        event = { "BufReadPost", "BufNewFile" },
 
-    config = function()
-      require("nvim-treesitter").setup({
-        ensure_installed = {
-          "html",
-          "javascript",
-          "typescript",
-          "tsx",
-          "css",
-          "json",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          "c",
-          "python",
-          "verilog",
-          "latex",
-        },
-        -- Run this if not automatically installed
-        -- :TSInstall html javascript typescript tsx css json
+        config = function()
+            require("nvim-treesitter").setup({
+                ensure_installed = {
+                    "html",
+                    "javascript",
+                    "typescript",
+                    "tsx",
+                    "css",
+                    "json",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "c",
+                    "python",
+                    "verilog",
+                    "latex",
+                },
+                -- Run this if not automatically installed
+                -- :TSInstall html javascript typescript tsx css json
+                additional_vim_regex_highlighting = false,
+                auto_install = true,
 
-        auto_install = true,
+                highlight = {
+                    enable = true,
+                },
 
-        highlight = {
-          enable = true,
-        },
-
-        indent = {
-          enable = true,
-          disable = { "python", "c" },
-        },
-      })
-    end,
-  },
+                indent = {
+                    enable = true,
+                    disable = { "python", "c" },
+                },
+            })
+        end,
+    },
 }
