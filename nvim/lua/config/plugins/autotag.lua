1431678df4d87d858bca4040604775da398c2e0d
Petr Zakopal <Petr.Zakopal@gmail.com>
2026-04-12 17:49:56 +0200

    neovim: removed autotag due to errors

--- nvim/lua/config/plugins/autotag.lua
+++ nvim/lua/config/plugins/autotag.lua
@@ -1,7 +1,9 @@
-    return{ {
-        'windwp/nvim-ts-autotag',
-        opts = {},
-        --ft = 'typescriptreact',
-        --requires = { { 'nvim-treesitter/nvim-treesitter' } },
-        --config = function() require('nvim-ts-autotag').setup() end,
-    }}
+    --return{ {
+    --    'windwp/nvim-ts-autotag',
+    --    opts = {},
+    --    --ft = 'typescriptreact',
+    --    --requires = { { 'nvim-treesitter/nvim-treesitter' } },
+    --    --config = function() require('nvim-ts-autotag').setup() end,
+    --}}
+
+    return{{}}
