db96ac801f4601c5a66c6bb94956c59fbe745cb7
Petr Zakopal <Petr.Zakopal@gmail.com>
2026-05-16 17:36:06 +0200

    arch: added retest mirrors for packages

---
+++ arch/retest-mirrors.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+# Keep sudo alive
+sudo -v
+
+# for cachy OS
+rate-mirrors cachyos | sudo tee /etc/pacman.d/cachyos-mirrorlist
+
+# for arch
+rate-mirrors arch | sudo tee /etc/pacman.d/mirrorlist
+
+# refresh the pacman database
+sudo pacman -Syy
